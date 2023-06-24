642995ead124629f6bd6da846d74248d934abcd5
yuan-cherish <940575974@qq.com>
2023-06-24 16:05:06 +0800

    changeFlaskAPIAddress

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ import ElementPlus from "element-plus"    // 导入element-ui的插件
 import "element-plus/dist/index.css"    // 一定要引入element的css样式，否则组件样式就会丢失
 import * as ElementPlusIconsVue from "@element-plus/icons-vue"      // 导入element-ui库中的所有图标并进行全局注册
 
-// const baseUrl = "https://47.102.104.144"
-const baseUrl = "https://172.30.217.233"
-const baseFlaskUrl = "https://127.0.0.1"
+const baseUrl = "https://47.102.104.144"
+// const baseUrl = "https://172.30.217.233"
+const baseFlaskUrl = "https://localhost"
 
 const app = createApp(App)
 
