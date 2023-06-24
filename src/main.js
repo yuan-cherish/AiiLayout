190787ea0bb9ef92840666531fb654e66b11ad24
yuan-cherish <940575974@qq.com>
2023-06-24 14:23:48 +0800

    01

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ import ElementPlus from "element-plus"    // 导入element-ui的插件
 import "element-plus/dist/index.css"    // 一定要引入element的css样式，否则组件样式就会丢失
 import * as ElementPlusIconsVue from "@element-plus/icons-vue"      // 导入element-ui库中的所有图标并进行全局注册
 
-// const baseUrl = "https://47.102.104.144"
-const baseUrl = "https://127.0.0.1"
+const baseUrl = "https://47.102.104.144"
+// const baseUrl = "https://localhost"
 
 const app = createApp(App)
 
